71837e9fb2be468aa5e0e5ed3085130b4d336e1a
ionmelail <149875580+ionmelail@users.noreply.github.com>
2025-04-25 14:18:12 +0300

    Update 03-install-nginx-ingress.sh

--- scripts/03-install-nginx-ingress.sh
+++ scripts/03-install-nginx-ingress.sh
@@ -40,13 +40,3 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "ClusterIP"}}' |
   echo "❌ Failed to patch argocd-server service. Exiting."
   exit 1
 }
-
-# Apply ArgoCD Ingress
-echo "📦 Applying ArgoCD Ingress resource..."
-kubectl apply -f argocd/argocd-server-ingress.yaml || {
-  echo "❌ Failed to apply ArgoCD Ingress. Exiting."
-  exit 1
-}
-
-echo "✅ ArgoCD is now accessible via:"
-echo "🔗 https://$LB_HOSTNAME"
