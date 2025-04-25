d2202ef628d34f7234c770ccbe1b96e376623db8
ionmelail <149875580+ionmelail@users.noreply.github.com>
2025-04-25 16:10:02 +0300

    Update 07-apply-argocd-app.sh

--- scripts/07-apply-argocd-app.sh
+++ scripts/07-apply-argocd-app.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "🚀 Applying ArgoCD application..."
 ACCOUNT_ID=${1:-"unknown"}
-sed "s|AWS_ACCOUNT_ID|$ACCOUNT_ID|g" manifests/application.yaml | kubectl apply -f -
+sed "s|AWS_ACCOUNT_ID|$ACCOUNT_ID|g" argocd/application.yaml | kubectl apply -f -
